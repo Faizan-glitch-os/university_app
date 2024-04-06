4b5fc7d7a8c02d10c31695050cb85e18ed9ab805
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-04-06 15:52:38 +0500

    starting university project

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'University',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'University Home Page'),
     );
   }
 }
@@ -30,65 +30,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _incrementCounter = 0;
-  int _decrementCounter = 0;
-
-  void _incrementCounterFunction() {
-    setState(() {
-      _incrementCounter++;
-    });
-  }
-
-  void _decrementCounterFunction() {
-    setState(() {
-      _decrementCounter--;
-    });
-  }
-
-  void _reloadNumbers() {
-    setState(() {
-      _incrementCounter = 0;
-      _decrementCounter = 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: _incrementCounterFunction,
-              child: const Text(
-                'Press to Increment',
-              ),
-            ),
-            Text(
-              '$_incrementCounter',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            ElevatedButton(
-              onPressed: _decrementCounterFunction,
-              child: const Text(
-                'Press to Decrement',
-              ),
-            ),
-            Text(
-              '$_decrementCounter',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _reloadNumbers,
-        child: const Icon(Icons.autorenew),
       ),
     );
   }
